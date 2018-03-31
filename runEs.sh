c28b06cfa6642366570c18d56374dd49a554453f
rainbow954 <rainbow954@163.com>
2018-03-31 16:16:44 +0800

    init

--- runEs.sh
+++ runEs.sh
@@ -4,3 +4,6 @@ sudo docker run -d --name es0 --publish 9200:9200 --publish 9300:9300 --net shad
 sudo docker run -d --name es1 --link es0 -e UNICAST_HOSTS=es0 --net shadownet --ip 172.19.0.31 registry.cn-hangzhou.aliyuncs.com/rainbow954/elasticsearch:latest
 
 sudo docker run -d --name es2 --link es0 -e UNICAST_HOSTS=es0 --net shadownet --ip 172.19.0.32 registry.cn-hangzhou.aliyuncs.com/rainbow954/elasticsearch:latest
+
+
+sudo docker run -d --name eshead -p 9100:9100 --net shadownet --ip 172.19.0.33 mobz/elasticsearch-head:5
