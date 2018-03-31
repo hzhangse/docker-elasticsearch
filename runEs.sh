56fe368a7a7172b2af3dd2503fcd2e2250c631bf
rainbow954 <rainbow954@163.com>
2018-03-31 15:14:25 +0800

    init

---
+++ runEs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo docker run -d --name es0 --publish 9200:9200 --publish 9300:9300 --net shadownet --ip 172.19.0.30 registry.cn-hangzhou.aliyuncs.com/rainbow954/elasticsearch:latest
+
+sudo docker run -d --name es1 --link es0 -e UNICAST_HOSTS=es0 --net shadownet --ip 172.19.0.31 registry.cn-hangzhou.aliyuncs.com/rainbow954/elasticsearch:latest
+
+sudo docker run -d --name es2 --link es0 -e UNICAST_HOSTS=es0 --net shadownet --ip 172.19.0.32 registry.cn-hangzhou.aliyuncs.com/rainbow954/elasticsearch:latest
