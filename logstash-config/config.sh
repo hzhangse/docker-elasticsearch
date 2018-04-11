6f3a628f693e1be806fb9941032a735585d29d81
rainbow954 <rainbow954@163.com>
2018-04-11 18:00:42 +0800

    test

--- logstash-config/config.sh
+++ logstash-config/config.sh
@@ -4,7 +4,7 @@ cat /usr/share/logstash/config/logstash-jdbc.conf.template | sed \
   -e "s|{{jdbc_connection_string}}|${jdbc_connection_string}|g" \
   -e "s|{{jdbc_user}}|${jdbc_user:root}|g" \
   -e "s|{{jdbc_password}}|${jdbc_password:root}|g" \
-  -e "s|{{elasticsearch.hosts}}|${elasticsearch.hosts}|g" \
+  -e "s|{{elasticsearch_hosts}}|${elasticsearch_hosts}|g" \
   -e "s|{{elasticsearch.index}}|${elasticsearch.index}|g" \
   -e "s|{{elasticsearch.index.type}}|${elasticsearch.index.type}|g" \
    > /usr/share/logstash/config/logstash-jdbc.conf
@@ -12,7 +12,7 @@ cat /usr/share/logstash/config/logstash-jdbc.conf.template | sed \
 
 
 cat /usr/share/logstash/config/jdbc/jdbc.sql.template | sed \
-  -e "s|{{jdbc.condition.sql}}|${jdbc.condition.sql}|g" \
+  -e "s|{{jdbc_condition_sql}}|${jdbc_condition_sql}|g" \
    > /usr/share/logstash/config/jdbc/jdbc.sql
 
 
