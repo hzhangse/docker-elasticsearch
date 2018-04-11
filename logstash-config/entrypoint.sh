1540f494943a8c4a202ab1ea360eb83177621037
rainbow954 <rainbow954@163.com>
2018-04-11 16:57:38 +0800

    test

--- logstash-config/entrypoint.sh
+++ logstash-config/entrypoint.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
-
-#exec /usr/share/logstash/config/config.sh "$@"
-exec "$@"
+# Map environment variables to entries in logstash.yml.
+# Note that this will mutate logstash.yml in place if any such settings are found.
+# This may be undesirable, especially if logstash.yml is bind-mounted from the
+# host system.
+env2yaml /usr/share/logstash/config/logstash.yml
+sh /usr/share/logstash/config/config.sh
+if [[ -z $1 ]] || [[ ${1:0:1} == '-' ]] ; then
+  exec logstash "$@"
+else
+  exec "$@"
+fi
