77e507dbe2db206261b03cd8751d1d13e23445c4
rainbow954 <rainbow954@163.com>
2018-05-21 23:51:40 +0800

    test

--- alluxio/fuse/bin/config.sh
+++ alluxio/fuse/bin/config.sh
@@ -4,4 +4,6 @@ cat /fuse/bin/alluxio-site.properties.template | sed \
   -e "s|{{alluxio_master}}|${alluxio_master}|g" \
    > /fuse/bin/alluxio-site.properties
 
+chmod a+x /fuse/bin/alluxio-site.properties    
+
 
